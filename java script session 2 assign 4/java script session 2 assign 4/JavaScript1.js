e40fd7f248f08e005e746e293bf459b3a45e52bc
praveenearle <praveen.earle@gmail.com>
2016-10-18 09:54:26 +0530

    java script s1a4
    java script s1a4

---
+++ java script session 2 assign 4/java script session 2 assign 4/JavaScript1.js
@@ -0,0 +1,61 @@
+﻿function checkbrackets(code) {
+    var length = code.length;
+    var delimiter = '';
+    var bracket = [];
+    var matching = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (var i = 0; i < length; i++) {
+        var char = code.charAt(i);
+
+        switch (char) {
+        case '"':
+        case "'":
+            if (delimiter)
+                if (char === delimiter)
+                    delimiter = '';
+            else delimiter = char;
+            break;
+        case '/':
+            var lookahead = code.charAt(++i);
+            switch (lookahead) {
+            case '/':
+            case '*':
+                delimiter = lookahead;
+            }
+            break;
+        case '*':
+            if (delimiter === '*' && code.charAt(++i) === '/') delimiter = '';
+            break;
+        case '\n':
+            if (delimiter === '/') delimiter = '';
+            break;
+        case '\\':
+            switch (delimiter) {
+            case '"':
+            case "'":
+                i++;
+            }
+            break;
+        case '(':
+        case '[':
+        case '{':
+            if (!delimiter) bracket.push(char);
+            break;
+        case ')':
+        case ']':
+        case '}':
+            if (!delimiter && bracket.length && matching[char] !== bracket.pop())
+                return 'incorrect';
+        }
+    }
+
+    return bracket.length ? 'incorrect' : 'correct';
+}
+console.log(checkbrackets('( ( a + b ) / 5 abd )'));
+console.log(checkbrackets('( b * ( c + d *2 / ( 2 + ( 12 abc / ( a + 3 ) ) ) )'));
+console.log(checkbrackets(') ( a + b ) )'));
+
---
+++ java script session 2 assign 4/java script session 2 assign 4/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("java_script_session_2_assign_4")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("java_script_session_2_assign_4")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d56b59c3-a2a8-4943-b2cc-2e0c10c6e6ec")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
